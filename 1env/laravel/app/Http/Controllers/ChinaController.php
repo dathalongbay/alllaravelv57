b8f78a51e1d57a388d6c71c763d20afb42516f3c
dat <dathalongbay@gmail.com>
2019-09-27 19:16:42 +0700

    111

---
+++ 1env/laravel/app/Http/Controllers/ChinaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChinaController extends Controller
+{
+    //
+
+    public function index() {
+
+        // Truyền biến từ controller xuống view
+        $data = array();
+        $data["dientich"] = "1 triệu km2";
+        $data["danso"] = "1,2 tỷ người";
+        $data["gdp"] = "1500 tỷ USD";
+
+        /**
+         * truyền 1 mảng từ controller xuống view
+         * và trong view mình sẽ truy cập tên biến chính là
+         * key của mảng truyền từ controller xuống
+         */
+
+        /**
+         * hàm view() có 2 tham số
+         * tham số 1 là đường dẫn của view tính từ folder resources/views/
+         * tham so 2 là biến truyền xuống view
+         */
+        return view("china", $data);
+    }
+}
