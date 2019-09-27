192ff53de6be06acc00000c82f38c4f64f0e00b3
dat <dathalongbay@gmail.com>
2019-09-27 20:10:15 +0700

    111

--- 1env/laravel/resources/views/japan.blade.php
+++ 1env/laravel/resources/views/japan.blade.php
@@ -10,5 +10,45 @@
     <p>dien tich japan : {{ $dientich }}  </p>
     <p>dan so japan : {{ $danso }}</p>
     <p>gdp japan : {{ $gdp }} </p>
+
+    <?php
+    $tuoi = 21;
+    if ($tuoi > 18) {
+        echo "<br> trưởng thành";
+    } else {
+        echo "<br> trẻ em";
+    }
+    ?>
+
+    @if ($tuoi > 18)
+        <br> trưởng thành
+    @else
+        <br> trẻ em
+    @endif
+
+
+    <?php
+    for($i = 1;$i <= 10;$i++) {
+        echo "<br>" . $i;
+    }
+    ?>
+
+    @for($i = 1;$i <= 10;$i++)
+        <br> {{ $i }}
+    @endfor
+
+    <?php
+    $array = ["ha noi", "quang ninh", "hai phong"];
+    foreach($array as $key => $value) {
+        echo "<br>" . $key . " - " . $value;
+    }
+    ?>
+
+    @foreach($array as $key => $value)
+        <br> {{ $key }} - {{ $value }}
+    @endforeach
+
+
+
 </body>
 </html>
