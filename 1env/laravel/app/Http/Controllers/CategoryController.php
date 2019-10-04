066743e76517794f41f64606d311f42f0ba6c020
dat <dathalongbay@gmail.com>
2019-10-04 19:36:37 +0700

    111

--- 1env/laravel/app/Http/Controllers/CategoryController.php
+++ 1env/laravel/app/Http/Controllers/CategoryController.php
@@ -20,6 +20,31 @@ class CategoryController extends Controller
         var_dump($id1);
         var_dump($id2);
         var_dump($id3);
-        echo "<br>" . __METHOD__; die;
+
+        // thử gõ : http://localhost/alllaravelv57/1env/laravel/public/category/1/1?abc=1
+
+        echo "<br>";
+        $uri = $request->path();
+        echo '<br> $request->path() => ' . $uri;
+        // Without Query String...
+        $url = $request->url();
+        echo '<br> $request->url() => ' . $url;
+        // With Query String...
+        $url = $request->fullUrl();
+        echo '<br> $request->fullUrl() => ' . $url;
+        $method = $request->method();
+        echo '<br> $request->method() => ' . $method;
+        echo '<br>' . __METHOD__; die;
+    }
+
+
+    public function create() {
+        return view("request.create");
+    }
+
+    public function store(Request $request) {
+
+
+
     }
 }
---
+++ 1env/laravel/resources/views/request/create.blade.php
@@ -0,0 +1,4 @@
+tạo 1 form dùng để đăng nhập đơn giản = html
+1 - username
+2 - password
+3 - nút submit đăng nhập
