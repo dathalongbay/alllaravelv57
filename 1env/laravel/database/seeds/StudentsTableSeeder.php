60e5ce278406826265aa98bd72655bbb7f74ffb6
dat <dathalongbay@gmail.com>
2019-10-11 20:11:29 +0700

    111

--- 1env/laravel/database/seeds/StudentsTableSeeder.php
+++ 1env/laravel/database/seeds/StudentsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class StudentsTableSeeder extends Seeder
 {
@@ -12,5 +14,13 @@ class StudentsTableSeeder extends Seeder
     public function run()
     {
         //
+
+        for($i = 1; $i <= 20; $i++) {
+            DB::table('students')->insert([
+                'student_name' => Str::random(10),
+                'student_location' => Str::random(10).'@gmail.com',
+            ]);
+        }
+
     }
 }
