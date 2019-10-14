0fc4ef856a821c4b411143faa941e9153085f060
dat <dathalongbay@gmail.com>
2019-10-14 20:31:52 +0700

    111

--- 1env/laravel/app/Http/Controllers/StudentsController.php
+++ 1env/laravel/app/Http/Controllers/StudentsController.php
@@ -49,4 +49,100 @@ class StudentsController extends Controller
         die;
 
     }
+
+    /**
+     * Học câu lệnh where trong DB
+     */
+    public function index3() {
+        $users = DB::table('students')->where('student_name', 'qLSKqHOUsO')->get();
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+    }
+
+    /**
+     * Học lấy 1 bản ghi duy nhất trong CSDL
+     */
+    public function index4() {
+        $users = DB::table('users')->first();
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+    }
+
+    /**
+     * Học câu lệnh where trong DB
+     */
+    public function index5() {
+        $users = DB::table('students')->where('id', '>', 3)->get();
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+    }
+
+    public function index6() {
+        $users = DB::table('students')->where('student_name', 'like', "W%")->get();
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+    }
+
+    public function index7() {
+
+        echo "<br>" . __METHOD__;
+        $users = DB::table('students')->orderBy('id', 'desc')->get();
+
+        /**
+         *
+         * Illuminate\Support\Collection Object
+         */
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+
+    }
+
+    public function index8() {
+
+        echo "<br>" . __METHOD__;
+        $users = DB::table('students')->orderBy('id', 'desc')->toSql();
+
+        /**
+         *
+         * Illuminate\Support\Collection Object
+         */
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+
+    }
+
+    public function index9() {
+
+        echo "<br>" . __METHOD__;
+        $users = DB::table('students')->where('student_name', 'like', "W%")->toSql();
+
+        /**
+         *
+         * Illuminate\Support\Collection Object
+         */
+
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+        die;
+
+    }
 }
